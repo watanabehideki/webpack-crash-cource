6ef71748c01885183ee5aaee2fbea91e8852d03e
watanabehideki <hideki.art.2280@gmail.com>
2022-02-27 16:18:31 +0900

    implement babel-loader & react development

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import _ from 'lodash'
+import React from "react"
+import ReacDOM from "react-dom"
 
-function component() {
-  const element = document.createElement('div')
-  const array = ['Hello', 'webpack', '!!!!!!']
-  element.innerHTML = _.join(array, ' ')
-  return element
-}
-
-document.body.appendChild(component())
+ReacDOM.render(
+  <div>Hello, React</div>,
+  document.getElementById('root')
+)
